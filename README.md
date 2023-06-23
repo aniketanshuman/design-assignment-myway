a5fa1a47735101dfceb1cf6cc2c8639f347a6fdc
aniketanshuman <aniket@iiitmanipur.ac.in>
2023-06-24 02:06:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+  # Assignment
+
+  1)Assignment of MyWays.ai
+  2) In the terminal, run `npm install`
+  3) Run `npm start` to view project in browser
+  
---
+++ src/pages/Home.js
@@ -0,0 +1,272 @@
+import "./Home.css";
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home-child" />
+      <div className="home-item" />
+      <div className="home-inner" />
+      <div className="ellipse-div" />
+      <div className="home-child1" />
+      <img className="vector-icon" alt="" src="/vector-4.svg" />
+      <div className="rectangle-div" />
+      <div className="home-parent">
+        <div className="home1">Home</div>
+        <div className="about-us">About us</div>
+        <div className="services">Services</div>
+        <div className="blog">Blog</div>
+      </div>
+      <div className="rectangle-parent">
+        <div className="group-child" />
+        <div className="contact-us">Contact us</div>
+      </div>
+      <div className="experienced-mobile-and-web-app-parent">
+        <div className="experienced-mobile-and-container">
+          <span>{`Experienced `}</span>
+          <span className="mobile-and-web">mobile and web</span>
+          <span> applications and website builders measuring.</span>
+        </div>
+        <div className="example-company-pvt">{`EXAMPLE COMPANY (PVT) LTD is a team of experienced mobile and web applications and website builders measuring dozens of completed projects. We build and develop mobile applications for several top platforms, including Android  & IOS. `}</div>
+        <div className="rectangle-group">
+          <div className="group-item" />
+          <div className="contact-us1">Contact us</div>
+          <div className="rectangle-container">
+            <div className="group-inner" />
+            <div className="view-more">View more</div>
+          </div>
+        </div>
+      </div>
+      <div className="bill-sitting-using-laptop-1-parent">
+        <img
+          className="bill-sitting-using-laptop-1"
+          alt=""
+          src="/bill-sitting-using-laptop-1@2x.png"
+        />
+        <img
+          className="succlent-angle-1"
+          alt=""
+          src="/succlent-angle-1@2x.png"
+        />
+        <img className="big-plant-1" alt="" src="/big-plant-1@2x.png" />
+        <img className="cuboid-1-icon" alt="" src="/cuboid-1@2x.png" />
+      </div>
+      <div className="line-div" />
+      <img className="home-child2" alt="" src="/vector-1.svg" />
+      <img className="home-child3" alt="" src="/vector-2.svg" />
+      <div className="lorem-ipsum-is-simply-dummy-te-parent">
+        <div className="lorem-ipsum-is">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's
+        </div>
+        <div className="lorem-ipsum-is1">{`Lorem Ipsum is simply dummy text of the printing. `}</div>
+      </div>
+      <div className="home-child4" />
+      <div className="group-parent">
+        <div className="group-div">
+          <div className="group-child1" />
+          <div className="web-application">Web Application</div>
+          <div className="lorem-ipsum-is2">Lorem Ipsum is simply</div>
+        </div>
+        <img
+          className="website-content-1-icon"
+          alt=""
+          src="/websitecontent-1.svg"
+        />
+      </div>
+      <div className="group-container">
+        <div className="rectangle-parent1">
+          <div className="group-child2" />
+          <div className="seo">SEO</div>
+          <div className="lorem-ipsum-is3">Lorem Ipsum is simply</div>
+        </div>
+        <img className="seo-tag-1-icon" alt="" src="/seotag-1.svg" />
+      </div>
+      <div className="group-parent1">
+        <div className="rectangle-parent1">
+          <div className="group-child3" />
+          <div className="arvr-solutions">AR/VR Solutions</div>
+          <div className="lorem-ipsum-is3">Lorem Ipsum is simply</div>
+        </div>
+        <img className="seo-tag-1-icon" alt="" src="/immersive-1.svg" />
+      </div>
+      <div className="mobile-applications-parent">
+        <div className="mobile-applications">Mobile Applications</div>
+        <div className="rectangle-parent3">
+          <div className="group-child4" />
+          <div className="lorem-ipsum-is5">Lorem Ipsum is simply</div>
+        </div>
+        <img className="coding-1-1" alt="" src="/coding-1-1.svg" />
+      </div>
+      <div className="recent-activities-1-parent">
+        <img
+          className="recent-activities-1"
+          alt=""
+          src="/17-recent-activities-1@2x.png"
+        />
+        <div className="group-child5" />
+        <div className="group-parent2">
+          <div className="lorem-ipsum-is-simply-dummy-te-group">
+            <div className="lorem-ipsum-is-container">
+              <span>Lorem Ipsum</span>
+              <span className="is-simply-dummy">{` is simply dummy text of the printing. `}</span>
+            </div>
+            <div className="ellipse-parent">
+              <div className="group-child6" />
+              <div className="lorem-ipsum-is-simply-dummy-te-container">
+                <div className="lorem-ipsum-is6">
+                  Lorem Ipsum is simply dummy text
+                </div>
+                <div className="lorem-ipsum-is7">
+                  Lorem Ipsum is simply dummy text
+                </div>
+              </div>
+            </div>
+            <div className="example-company-pvt1">{`EXAMPLE COMPANY (PVT) LTD is a team of experienced mobile and web applications and website builders measuring dozens of completed projects. We build and develop mobile applications for several top platforms, including Android  & IOS. `}</div>
+          </div>
+          <img className="happy-1-icon" alt="" src="/happy-1.svg" />
+        </div>
+      </div>
+      <div className="ellipse-group">
+        <div className="group-child7" />
+        <div className="group-parent3">
+          <div className="lorem-ipsum-is-simply-dummy-te-group">
+            <div className="lorem-ipsum-is-container">
+              <span>Lorem Ipsum</span>
+              <span className="is-simply-dummy">{` is simply dummy text of the printing. `}</span>
+            </div>
+            <div className="ellipse-parent">
+              <div className="group-child8" />
+              <div className="lorem-ipsum-is-simply-dummy-te-container">
+                <div className="lorem-ipsum-is6">
+                  Lorem Ipsum is simply dummy text
+                </div>
+                <div className="lorem-ipsum-is7">
+                  Lorem Ipsum is simply dummy text
+                </div>
+              </div>
+            </div>
+            <div className="example-company-pvt1">{`EXAMPLE COMPANY (PVT) LTD is a team of experienced mobile and web applications and website builders measuring dozens of completed projects. We build and develop mobile applications for several top platforms, including Android  & IOS. `}</div>
+          </div>
+          <img className="happy-1-icon" alt="" src="/happy-11.svg" />
+        </div>
+        <img
+          className="website-mockup-png-10-1-icon"
+          alt=""
+          src="/websitemockuppng10-1@2x.png"
+        />
+      </div>
+      <div className="group-parent4">
+        <div className="group-wrapper">
+          <div className="lorem-ipsum-is-simply-dummy-te-group">
+            <div className="lorem-ipsum-is-simply-dummy-te-group">
+              <div className="lorem-ipsum-is-container">
+                <span>Lorem Ipsum</span>
+                <span className="is-simply-dummy">{` is simply dummy text of the printing. `}</span>
+              </div>
+              <div className="ellipse-parent">
+                <div className="group-child9" />
+                <div className="lorem-ipsum-is-simply-dummy-te-container">
+                  <div className="lorem-ipsum-is6">
+                    Lorem Ipsum is simply dummy text
+                  </div>
+                  <div className="lorem-ipsum-is7">
+                    Lorem Ipsum is simply dummy text
+                  </div>
+                </div>
+              </div>
+              <div className="example-company-pvt1">{`EXAMPLE COMPANY (PVT) LTD is a team of experienced mobile and web applications and website builders measuring dozens of completed projects. We build and develop mobile applications for several top platforms, including Android  & IOS. `}</div>
+            </div>
+            <img className="happy-1-icon" alt="" src="/happy-12.svg" />
+          </div>
+        </div>
+        <img
+          className="focus-parallax-hero-1920x1000-icon"
+          alt=""
+          src="/focusparallaxhero-1920x1000-arvr-0917-layer1guy-1@2x.png"
+        />
+      </div>
+      <div className="home-child5" />
+      <div className="home-child6" />
+      <div className="group-parent6">
+        <div className="rectangle-parent4">
+          <div className="group-child10" />
+          <div className="example-company-pvt4">{`EXAMPLE COMPANY PVT LTD is a team of experienced mobile and web applications and website builders measuring dozens of completed projects. We build and develop mobile applications for several top platforms, including Android  & IOS. `}</div>
+          <div className="lorem-ipsum-is12">
+            Lorem Ipsum is simply dummy text.
+          </div>
+          <div className="rectangle-parent5">
+            <div className="group-child11" />
+            <div className="view-more1">View More</div>
+          </div>
+        </div>
+        <div className="rectangle-parent6">
+          <div className="group-child12" />
+          <div className="lorem-ipsum-is13">
+            Lorem Ipsum is simply dummy text.
+          </div>
+          <div className="example-company-pvt4">{`EXAMPLE COMPANY PVT LTD is a team of experienced mobile and web applications and website builders measuring dozens of completed projects. We build and develop mobile applications for several top platforms, including Android  & IOS. `}</div>
+          <div className="rectangle-parent7">
+            <div className="group-child13" />
+            <div className="view-more1">View More</div>
+          </div>
+        </div>
+      </div>
+      <img className="bell-angle-1" alt="" src="/bell-angle-1@2x.png" />
+      <div className="you-will-be-in-good-company-parent">
+        <div className="you-will-be">You will be in good Company</div>
+        <div className="beneoshop-parent">
+          <img className="beneoshop-icon" alt="" src="/beneoshop@2x.png" />
+          <img className="beneoshop-icon" alt="" src="/caspio@2x.png" />
+          <img className="beneoshop-icon" alt="" src="/hypergrid@2x.png" />
+          <img className="beneoshop-icon" alt="" src="/leotrippi@2x.png" />
+        </div>
+      </div>
+      <div className="rectangle-parent8">
+        <div className="group-child14" />
+        <div className="lorem-ipsum-is-simply-dummy-te-parent5">
+          <div className="lorem-ipsum-is14">{`Lorem Ipsum is simply dummy text of the printing. `}</div>
+          <div className="rectangle-parent9">
+            <div className="group-child15" />
+            <div className="enter-your-email">Enter your email</div>
+          </div>
+          <div className="rectangle-parent10">
+            <div className="group-child16" />
+            <div className="subscribe">SUBSCRIBE</div>
+          </div>
+        </div>
+        <img className="star-icon" alt="" src="/star-1@2x.png" />
+        <img className="pai-chart-1" alt="" src="/pai-chart-1@2x.png" />
+      </div>
+      <div className="group-parent7">
+        <div className="group-frame">
+          <div className="group-wrapper1">
+            <div className="group-wrapper1">
+              <div className="lorem-ipsum-is15">{`Lorem Ipsum is simply dummy text of the printing and typesetting industry. `}</div>
+              <div className="lorem-portfolio-careers-container">
+                <p className="lorem">Lorem</p>
+                <p className="lorem">Portfolio</p>
+                <p className="lorem">Careers</p>
+                <p className="lorem">Contact us</p>
+              </div>
+              <div className="lorem1">@Lorem</div>
+              <img className="group-icon" alt="" src="/group-12.svg" />
+              <div className="about-us1">About us</div>
+              <div className="lorem-ipsum-is-simply-dummy-te-parent7">
+                <div className="lorem-ipsum-is16">{`Lorem Ipsum is simply dummy text of the printing and typesetting industry. `}</div>
+                <div className="div">+908 89097 890</div>
+                <div className="contact-us3">Contact us</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="group-child17" />
+        <div className="copyright-2021">
+          Copyright ® 2021 Lorem All rights Rcerved
+        </div>
+      </div>
+      <b className="logo">LOGO</b>
+      <b className="logo1">LOGO</b>
+    </div>
+  );
+};
+
+export default Home;
